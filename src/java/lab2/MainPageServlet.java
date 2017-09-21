03b44c5bee41f33262852b056dd24df80c0229b6
721292 <721292@ELF6IT216348.ACDM.DS.SAIT.CA>
2017-09-21 09:37:30 -0600

    Week 2 Lab 2 Login page 1. Generated servlet and jsp 2. Java class created 3. Logged out message not displayed?

---
+++ src/java/lab2/MainPageServlet.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 721292
+ */
+public class MainPageServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        request.setAttribute("message", "User Logged out.");
+        getServletContext().getRequestDispatcher("/WEB-INF/Login.jsp").forward(request, response);
+    }
+
+}
---
+++ web/WEB-INF/Login.jsp
@@ -0,0 +1,26 @@
+<%-- 
+    Document   : Login
+    Created on : Sep 20, 2017, 2:36:08 PM
+    Author     : 721292
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Lab2 Login Page</title>
+    </head>
+    <body>
+        <h1>LOGIN</h1>
+        <h2>${message}</h2>
+        <div>
+            <form action="Login" method="post">
+                Username :   <input type="text" name="usern" value="${param.usern}"><br>
+                Password :   <input type="text" name="passw" value="${param.passw}"><br>
+                <input type="submit" value="Login">
+            </form>
+        </div>
+        ${errorMessage}
+    </body>
+</html>
